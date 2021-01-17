4caeb1985793af76246fe70b94c650c1c6a0719b
mktcode <kontakt@markus-kottlaender.de>
2021-01-17 17:54:30 +0100

    added twitte post job

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const createReleaseRequest = require('./endpoints/release').createRequest
 const createClaimRequest = require('./endpoints/claim').createRequest
 const createGraphqlRequest = require('./endpoints/graphql').createRequest
 const createNotifyRequest = require('./endpoints/notify').createRequest
+const createTwitterPostRequest = require('./endpoints/twitter-post').createRequest
 
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -46,6 +47,13 @@ app.post('/graphql', (req, res) => {
 app.post('/notify', (req, res) => {
   console.log('POST Data: ', req.body)
   createNotifyRequest(req.body, (status, result) => {
+    res.status(status).json(result)
+  })
+})
+
+app.post('/twitter-post', (req, res) => {
+  console.log('POST Data: ', req.body)
+  createTwitterPostRequest(req.body, (status, result) => {
     res.status(status).json(result)
   })
 })
---
+++ endpoints/twitter-post.js
@@ -0,0 +1,113 @@
+require('dotenv').config()
+const Twitter = require('twitter-lite')
+const web3 = require('web3')
+const { Requester, Validator } = require('@chainlink/external-adapter')
+
+// Define custom error scenarios for the API.
+// Return true for the adapter to retry.
+const customError = (data) => {
+  if (data.Response === 'Error') return true
+  return false
+}
+
+// Define custom parameters to be used by the adapter.
+// Extra parameters can be stated in the extra object,
+// with a Boolean value indicating whether or not they
+// should be required.
+const customParams = {
+  issueId: ['issueId'],
+  amount: ['amount']
+}
+
+const twApp = new Twitter({
+  subdomain: 'api',
+  version: '1.1',
+  consumer_key: process.env.TWITTER_API_KEY,
+  consumer_secret: process.env.TWITTER_API_SECRET,
+  access_token_key: process.env.TWITTER_APP_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_APP_SECRET
+})
+
+const createRequest = (input, callback) => {
+  // The Validator helps you validate the Chainlink request data
+  const validator = new Validator(callback, input, customParams)
+  const jobRunID = validator.validated.id
+  const url = 'https://api.github.com/graphql'
+  const issueId = validator.validated.data.issueId
+  const amountFormatted = Number(web3.utils.fromWei(validator.validated.data.amount, 'ether'))
+
+  const headers = {
+    Authorization: 'bearer ' + process.env.GITHUB_PERSONAL_ACCESS_TOKEN
+  }
+
+  // Axios config
+  const config = {
+    url,
+    headers,
+    method: 'POST',
+    data: {
+      query: `query {
+        node (id: "${issueId}") {
+          ... on Issue {
+            url
+          }
+        }
+      }`
+    }
+  }
+
+  // The Requester allows API calls be retry in case of timeout
+  // or connection failure
+  Requester.request(config, customError)
+    .then(async response => {
+      // remove redundant object node
+      response.data = response.data.data
+      console.log(response.data)
+
+      if (response.data.node.url) {
+        const tweet = await twApp.post('statuses/update', {
+          status: `${amountFormatted} #ETH was deposited on this issue: ${response.data.node.url} #ethereum #github #opensource #octobay`
+        })
+        delete response.data.node
+        response.data.result = tweet.id
+        callback(response.status, Requester.success(jobRunID, response))
+      } else {
+        callback(500, Requester.errored(jobRunID, { error: `Issue not found: ${issueId}` }))
+      }
+    })
+    .catch(error => {
+      callback(500, Requester.errored(jobRunID, error))
+    })
+}
+
+// This is a wrapper to allow the function to work with
+// GCP Functions
+exports.gcpservice = (req, res) => {
+  createRequest(req.body, (statusCode, data) => {
+    res.status(statusCode).send(data)
+  })
+}
+
+// This is a wrapper to allow the function to work with
+// AWS Lambda
+exports.handler = (event, context, callback) => {
+  createRequest(event, (statusCode, data) => {
+    callback(null, data)
+  })
+}
+
+// This is a wrapper to allow the function to work with
+// newer AWS Lambda implementations
+exports.handlerv2 = (event, context, callback) => {
+  createRequest(JSON.parse(event.body), (statusCode, data) => {
+    callback(null, {
+      statusCode: statusCode,
+      body: JSON.stringify(data),
+      isBase64Encoded: false
+    })
+  })
+}
+
+// This allows the function to be exported for testing
+// or for running in express
+module.exports.createRequest = createRequest
