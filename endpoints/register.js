92c30a68c2994d1e5691ad9f2351f2dd2f8fee5f
mktcode <kontakt@markus-kottlaender.de>
2021-03-10 23:55:02 +0100

    made register endpoint return name for address

--- endpoints/register.js
+++ endpoints/register.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const { Requester, Validator } = require('@chainlink/external-adapter')
+const truncateBytes = require('truncate-utf8-bytes')
 
 // Define custom error scenarios for the API.
 // Return true for the adapter to retry.
@@ -13,7 +14,7 @@ const customError = (data) => {
 // with a Boolean value indicating whether or not they
 // should be required.
 const customParams = {
-  githubUser: ['githubUser'],
+  githubUserId: ['githubUserId'],
   ethAddress: ['ethAddress']
 }
 
@@ -22,7 +23,7 @@ const createRequest = (input, callback) => {
   const validator = new Validator(callback, input, customParams)
   const jobRunID = validator.validated.id
   const url = 'https://api.github.com/graphql'
-  const githubUser = validator.validated.data.githubUser
+  const githubUserId = validator.validated.data.githubUserId
   const ethAddress = '0x' + BigInt(validator.validated.data.ethAddress).toString(16).padStart(40, '0')
 
   const headers = {
@@ -35,11 +36,19 @@ const createRequest = (input, callback) => {
     headers,
     method: 'POST',
     data: {
-      query: `query {
-        repository(owner: "${githubUser}", name: "${ethAddress}") {
-          name
+      query: `query($githubUserId:ID!, $ethAddress:String!) {
+        node(id: $githubUserId) {
+          ... on User {
+            repository(name: $ethAddress) {
+              description
+            }
+          }
         }
-      }`
+      }`,
+      variables: {
+        githubUserId,
+        ethAddress
+      }
     }
   }
 
@@ -50,12 +59,17 @@ const createRequest = (input, callback) => {
       // remove redundant object node
       response.data = response.data.data
 
-      if (!response.data.repository) {
+
+      if (!response.data.node) {
         // Error 1: Repository not found
         callback(500, Requester.errored(jobRunID, { registrationError: `Repository (${ethAddress}) not found.` }))
       } else {
-        delete response.data.repository
-        response.data.result = true
+        let addressName = response.data.node.repository.desciption
+        if (!addressName) {
+          addressName = ethAddress.substr(0, 5) + '...' + ethAddress.substr(38)
+        }
+        response.data.result = truncateBytes(addressName, 32)
+        delete response.data.node
         callback(response.status, Requester.success(jobRunID, response))
       }
     })
